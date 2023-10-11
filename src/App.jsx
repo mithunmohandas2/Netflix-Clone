ff3b3299e926d279bce8faa6af65716fc6bc4a92
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-10-12 00:16:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Intro from './Components/Intro/intro'
+function App() {
+  return (
+    <>
+      <div>
+      <Intro/>
+      </div>
+    </>
+  )
+}
+
+export default App
