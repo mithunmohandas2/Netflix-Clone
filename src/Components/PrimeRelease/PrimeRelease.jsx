27b3c4eb6b648f4353550128c4fb187279c0ba1e
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-10-13 03:50:33 +0530

    Added Trailer button for banners

--- src/Components/PrimeRelease/PrimeRelease.jsx
+++ src/Components/PrimeRelease/PrimeRelease.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import './PrimeRelease.css'
 import axios from '../../axios'
-import { API_KEY, imgUrl } from '../../constants/constant'
+import { baseUrl,API_KEY, imgUrl } from '../../constants/constant'
+import YouTube from 'react-youtube'
 
 function PrimeRelease() {
   const [Movie1, setMovie1] = useState({})
   const [Movie2, setMovie2] = useState({})
   const [Movie3, setMovie3] = useState({})
+  const [movieID, setMovieID] = useState(null)
 
   useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
@@ -16,10 +18,29 @@ function PrimeRelease() {
         setMovie3(response.data.results[2]);
         // console.log(response.data.results[0])
       })
-      .catch((err)=>{
-        alert('Network Error')
-    })
+      .catch((err) => {
+        console.log(err)
+      })
   }, [])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: { autoplay: 1 },
+  };
+
+  function trailerHandler(id) {
+    console.log(id)
+    axios.get(`${baseUrl}movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+        .then((res) => {
+            // setMovieID(res.data.results);
+            console.log(res.data.results.length > 0 ? res.data.results[0].key : null);
+            setMovieID(res.data.results.length > 0 ? res.data.results[0].key : null);
+        }).catch((err)=>{
+            console.log(err)
+            setMovieID(null);
+        })
+}
 
   return (
     <div id="carouselExampleCaptions" className="carousel slide " data-bs-ride="carousel">
@@ -37,8 +58,7 @@ function PrimeRelease() {
             <p>{Movie1 ? Movie1.overview : ''}</p>
 
             <div className='banner_buttons my-3' >
-              <button className='btn btn-danger m-1 px-4' >Play</button>
-              <button className='btn btn-danger m-1 px-4' >My list</button>
+              <button className='btn btn-danger m-1 px-4' onClick={()=>trailerHandler(Movie1.id)} >Play Trailer</button>
             </div>
           </div>
         </div>
@@ -46,28 +66,27 @@ function PrimeRelease() {
         <div className="carousel-item">
           <img src={`${imgUrl + Movie2.backdrop_path}`} className="d-block w-100" alt="Movie-2_banner" />
           <div className="carousel-caption d-none d-md-block">
-              <h2>{Movie1 ? Movie2.title : ''}</h2>
-              <h5 className='mb-4'>Rating : {Movie1 ? Movie2.vote_average : ''} / 10</h5>
-              <p>{Movie1 ? Movie2.overview : ''}</p>
+            <h2>{Movie1 ? Movie2.title : ''}</h2>
+            <h5 className='mb-4'>Rating : {Movie1 ? Movie2.vote_average : ''} / 10</h5>
+            <p>{Movie1 ? Movie2.overview : ''}</p>
 
-              <div className='banner_buttons my-3' >
-                <button className='btn btn-danger m-1 px-4' >Play</button>
-                <button className='btn btn-danger m-1 px-4' >My list</button>
-              </div>
+            <div className='banner_buttons my-3' >
+              <button className='btn btn-danger m-1 px-4' onClick={()=>trailerHandler(Movie2.id)} >Play Trailer</button>
+
             </div>
+          </div>
         </div>
 
         <div className="carousel-item">
           <img src={`${imgUrl + Movie3.backdrop_path}`} className="d-block w-100" alt="Movie-3_banner" />
           <div className="carousel-caption d-none d-md-block">
-          <h2>{Movie1 ? Movie3.title : ''}</h2>
-              <h5 className='mb-4'>Rating : {Movie3 ? Movie3.vote_average : ''} / 10</h5>
-              <p>{Movie1 ? Movie3.overview : ''}</p>
+            <h2>{Movie1 ? Movie3.title : ''}</h2>
+            <h5 className='mb-4'>Rating : {Movie3 ? Movie3.vote_average : ''} / 10</h5>
+            <p>{Movie1 ? Movie3.overview : ''}</p>
 
-              <div className='banner_buttons my-3' >
-                <button className='btn btn-danger m-1 px-4' >Play</button>
-                <button className='btn btn-danger m-1 px-4' >My list</button>
-              </div>
+            <div className='banner_buttons my-3' >
+              <button className='btn btn-danger m-1 px-4' onClick={()=>trailerHandler(Movie3.id)} >Play Trailer</button>
+            </div>
           </div>
         </div>
       </div>
@@ -80,6 +99,10 @@ function PrimeRelease() {
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
       </button>
+
+      {movieID && <YouTube videoId={movieID} opts={opts} />}
+
+
     </div>
   )
 }
--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -7,5 +7,3 @@ export const imgUrl = 'https://image.tmdb.org/t/p/original';
 
 export const originalsUrl =`${baseUrl}discover/tv?api_key=${API_KEY}&with_networks=213`
 export const ActionUrl =`${baseUrl}discover/movie?api_key=${API_KEY}&with_genres=28`
-
-export const YoutubeBaseUrl = 'https://www.youtube.com/watch?v='
