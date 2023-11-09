cf73d077305c821ad5a04f0111562219a4a2d227
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-11-09 14:39:46 +0530

    vercel host fix

--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -1,5 +1,5 @@
 //API Key in Secret Data File
-import { TMDB_API_KEY } from "../../secretData";
+import { TMDB_API_KEY } from "../../TMDB";
 
 export const baseUrl = 'https://api.themoviedb.org/3/';
 export const API_KEY =  TMDB_API_KEY;
